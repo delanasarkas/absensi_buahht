d9b6b180bc36123a8c7ee2cebfb9a1ed09d63455
Delana Mahfiz <delsarkasmahfiz@gmail.com>
2021-10-30 02:24:41 +0700

    new db

--- buah_ht.sql
+++ buah_ht.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 26, 2021 at 03:54 PM
+-- Generation Time: Oct 29, 2021 at 09:23 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -91,6 +91,12 @@ CREATE TABLE `users` (
   `username` varchar(100) NOT NULL,
   `alamat` varchar(255) NOT NULL,
   `no_tlp` varchar(13) NOT NULL,
+  `agama` varchar(50) DEFAULT NULL,
+  `status_tinggal` varchar(100) DEFAULT NULL,
+  `gol_darah` varchar(50) DEFAULT NULL,
+  `warga` varchar(50) DEFAULT NULL,
+  `ktp` varchar(50) DEFAULT NULL,
+  `npwp` varchar(50) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `is_active` enum('true','false','') NOT NULL,
   `created_at` datetime NOT NULL,
@@ -101,10 +107,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id_users`, `id_role`, `id_divisi`, `nik`, `email`, `username`, `alamat`, `no_tlp`, `password`, `is_active`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, '81828120109', 'rinda@gmail.com', 'Rinda', 'JL.PASAR SASAK CILEDUK JAYA', '0812812828', '2807266b476de7e2969e5f6669a225ac', 'true', '2021-10-24 16:46:10', '0000-00-00 00:00:00'),
-(2, 2, 2, '20161477', 'Solehun@gmail.com', 'Solehun', 'JL.PAJAJARAN BARAT RAYA INDAH', '08921727127', 'c890411b93dcaadfdb30ffe2752d96d5', 'true', '2021-10-25 10:32:36', NULL),
-(3, 2, 2, '2016148799', 'neiji@gmail.com', 'Neiji', 'JL.PANGUYUBUAN RAYA BARAT 2', '089182718188', '3d3eb22ed9e275a262a6eadded25eebc', 'true', '2021-10-26 07:59:42', NULL);
+INSERT INTO `users` (`id_users`, `id_role`, `id_divisi`, `nik`, `email`, `username`, `alamat`, `no_tlp`, `agama`, `status_tinggal`, `gol_darah`, `warga`, `ktp`, `npwp`, `password`, `is_active`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '81828120109', 'rinda@gmail.com', 'Rinda', 'JL.PASAR SASAK CILEDUK JAYA', '0812812828', NULL, NULL, NULL, NULL, NULL, NULL, '2807266b476de7e2969e5f6669a225ac', 'true', '2021-10-24 16:46:10', '0000-00-00 00:00:00'),
+(3, 2, 2, '2016148799', 'neiji@gmail.com', 'Neiji', 'JL.PANGUYUBUAN RAYA BARAT 2', '089182718188', NULL, NULL, NULL, NULL, NULL, NULL, '3d3eb22ed9e275a262a6eadded25eebc', 'true', '2021-10-26 07:59:42', NULL),
+(4, 2, 2, '121212', 'njin@gmail.com', 'Njin', 'JL.PASURUAN BARAT', '0812818281', 'Islam', 'Orangtua', 'A', 'Indonesia', '01281821212', '8121827172', 'a889aa282c4e893bbe5ce82efda5804a', 'true', '2021-10-29 13:07:56', NULL);
 
 --
 -- Indexes for dumped tables
@@ -164,7 +170,7 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_users` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_users` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
